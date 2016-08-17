1f0303bef681511cdebb6e127a966ca0f6defcae
samueldavidcrawford <samueldavidcrawford1@gmail.com>
2016-08-17 09:23:30 -0500

    Initial Commit

---
+++ Transient/Controllers/VehiclesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Transient.Models;
+
+namespace Transient.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        // GET: Vehicles
+        public ActionResult Random()
+        {
+            var vehicle = new Vehicle() { Name = "Phantom" };
+
+            return View(vehicle);
+        }
+    }
+}
