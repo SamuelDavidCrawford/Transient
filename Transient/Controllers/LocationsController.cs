1d8c1622d6ab000403ca9703a5470f5cb640f71f
samueldavidcrawford <samueldavidcrawford1@gmail.com>
2016-08-18 14:36:33 -0500

    Added  Locations Index and Details pages

---
+++ Transient/Controllers/LocationsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Transient.Models;
+
+namespace Transient.Controllers
+{
+    public class LocationsController : Controller
+    {
+        // GET: Locations
+        public ActionResult Index()
+        {
+            var locations = GetLocations();
+
+            return View(locations);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var location = GetLocations().SingleOrDefault(l => l.Id == id);
+
+            if (location == null)
+                return HttpNotFound();
+
+            return View(location);
+
+        }
+
+        private IEnumerable<Location> GetLocations()
+        {
+            return new List<Location>
+            {
+                new Location { Name = "Bucktown", Id = 1 },
+                new Location { Name = "Wicker Park", Id = 2 }
+            };
+
+        }
+    }
+
+}
---
+++ Transient/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transient.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Vehicle> Vehicles { get; set; }
+
+    }
+}
