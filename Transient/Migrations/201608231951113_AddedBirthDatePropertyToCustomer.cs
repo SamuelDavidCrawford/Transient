0c75eb7b0dbfcb8d57bc29d8545dc592dedb5458
samueldavidcrawford <samueldavidcrawford1@gmail.com>
2016-08-23 15:11:26 -0500

    Added Birthdate for Customers

---
+++ Transient/Migrations/201608231951113_AddedBirthDatePropertyToCustomer.cs
@@ -0,0 +1,18 @@
+namespace Transient.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedBirthDatePropertyToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "BirthDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "BirthDate");
+        }
+    }
+}
---
+++ Transient/Migrations/201608231959366_AddedBirthDatePropertyToCustomer1.cs
@@ -0,0 +1,18 @@
+namespace Transient.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedBirthDatePropertyToCustomer1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "BirthDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "BirthDate", c => c.DateTime(nullable: false));
+        }
+    }
+}
