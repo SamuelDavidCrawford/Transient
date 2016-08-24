bf2b685ce2e62f53d51c5eaf58a172b17fe3a36b
samueldavidcrawford <samueldavidcrawford1@gmail.com>
2016-08-24 14:50:21 -0500

    Customer DB and Forms working

---
+++ Transient/ViewModels/VehicleFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Transient.Models;
+
+namespace Transient.ViewModels
+{
+    public class VehicleFormViewModel
+    {
+        public IEnumerable<VehicleType> VehicleType { get; set; }
+        public Vehicle Vehicle { get; set; }
+
+    }
+}
